c6b7b5e6698a1a37bcce3d7e11129dc765cbe487
Seshadri Mahalingam <seshadri@berkeley.edu>
2011-05-28 17:12:31 -0700

    Don't copy on a mouse dragged region, make the cursor blink (in X), display "special" frames like *grep* and *VC-log* in their own popup frames.
    Also set keyboard scrolling ot 1 step at a time!

--- general.el
+++ general.el
@@ -1,6 +1,6 @@
 ;; general.el
 
-;; s/yes/y and s/no/n
+;; s/yes/y/g and s/no/n/g
 (fset 'yes-or-no-p 'y-or-n-p)
 
 ;; Decrease mouse wheel jerkiness
@@ -9,3 +9,21 @@
 
 ;; Default fill column: 80 characters
 (setq-default fill-column 80)
+
+;; Disable copy on mouse drag region
+(setq-default mouse-drag-copy-region nil)
+
+;; Blinking cursor
+(blink-cursor-mode t)
+
+;; popup a speedbar
+;; (when window-system ; not just X
+;;   (speedbar 1))
+
+;; Put special stuff in their own frames
+(setq special-display-buffer-names
+      (nconc '("*Backtrace*" "*VC-log*" "*compilation*" "*grep*")
+             special-display-buffer-names))
+
+;; keyboard scroll one line at a time
+(setq scroll-step 1)
