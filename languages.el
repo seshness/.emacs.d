ce659e42e6f4082d3b8f675fc5186922f4410483
Seshadri Mahalingam <seshadri@berkeley.edu>
2011-05-28 17:39:48 -0700

    Set the default C style to Google's C style (it's their .el file) - 2 space indent

--- languages.el
+++ languages.el
@@ -8,3 +8,8 @@
 
 ;; Change default spacing from 4 -> 2 spaces in C
 (setq c-basic-offset 2)
+
+;; Set google's c-style
+(require 'google-c-style)
+(add-hook 'c-mode-common-hook 'google-set-c-style)
+(add-hook 'c-mode-common-hook 'google-make-newline-indent)
