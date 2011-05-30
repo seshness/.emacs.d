eea05755462b26be581fbb5bc6233945f86b15c1
Seshadri Mahalingam <seshadri@fb.com>
2011-05-30 14:55:25 -0700

    Added php-mode

--- languages.el
+++ languages.el
@@ -3,8 +3,13 @@
 
 (add-to-list 'load-path (concat emacs-config-home "/languages/"))
 
+(autoload 'php-mode "php-mode" "Major mode for editing php code." t)
+
 ;; Yacc/Bison mode
 (load "yacc.el")
 
 ;; Change default spacing from 4 -> 2 spaces in C
 (setq c-basic-offset 2)
+
+(add-to-list 'auto-mode-alist
+	     '("\\.php[34]?\\'\\|\\.phtml\\'" . php-mode))
